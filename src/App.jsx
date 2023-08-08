60a131874e2d2bf2aa30ed0b4819cf86ac333590
budfy <serdyuk.ro@gmail.com>
2023-08-08 21:44:37 +0300

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import { Helmet } from 'react-helmet';
 
 export default function App() {
   return (
-    // <Routes>
-    //   <Route path="/">
-    <div className="App">
+    <>
       <Helmet>
         <meta charset="utf-8"></meta>
         <meta name="description" content="!!!" />
         <meta name="keywords" content="!!!" />
-    <meta content="width=device-width,initial-scale=1"
+        <meta content="width=device-width,initial-scale=1"
           name="viewport"></meta>
         {/* <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> */}
         <title>Sestry</title>
       </Helmet>
+    {/* <Routes> */}
+    {/* <Route path="/"> */}
+    <div className="App">
       HELLO!
     </div>
-    //   </Route>
-    // </Routes>
+    {/* </Route> */}
+    {/* </Routes> */}
+  </>
   );
 }
