ee3d65a773e657a671c50341cc2a5df395094bde
Roman Serdiuk <budfy@MacBook-Pro-uastar1502.local>
2023-08-21 17:00:04 +0300

    Styled

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { Helmet } from 'react-helmet';
+import favicon_png from './assets/icons/favicon.png';
+import favicon_svg from './assets/icons/favicon.svg';
 
 export default function App() {
   return (
@@ -15,7 +17,8 @@ export default function App() {
         <meta name="keywords" content="!!!" />
         <meta content="width=device-width,initial-scale=1"
           name="viewport"></meta>
-        {/* <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> */}
+        <link rel="shortcut icon" href={favicon_svg} type="image/x-icon" />
+        <link rel="shortcut icon" href={favicon_png} type="image/png" />
         <title>Sestry</title>
       </Helmet>
       <Header />
